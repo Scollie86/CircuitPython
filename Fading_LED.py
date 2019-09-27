e0d112813039a6a2816198146b151f2e4a5d5843
Seth M Collier <scollie86@ccs.local>
2019-09-27 09:52:19 -0400

    i added my Fading LED file

---
+++ Fading_LED.py
@@ -0,0 +1,20 @@
+import time
+ import board
+import pulseio
+
+
+
+digital_leds = DigitalInOut(board.D2)
+digital_leds.direction = Direction.OUTPUT
+
+
+while True:
+
+
+    # reverse the direction of the fading at the ends of the fade:
+    if brightness <= 0:
+        fade_amount = -fade_amount
+        counter += 1
+    elif brightness >= 65535:
+        fade_amount = -fade_amount
+        counter += 1
